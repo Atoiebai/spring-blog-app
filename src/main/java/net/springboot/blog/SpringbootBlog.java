f9e6694fa6d646ce41720698a82ac792eaae93fc
Yusubov Islam <yusubov9999@mail.ru>
2021-01-13 10:57:07 +0500

    insert i18n

--- src/main/java/net/springboot/blog/SpringbootBlog.java
+++ src/main/java/net/springboot/blog/SpringbootBlog.java
@@ -3,18 +3,11 @@ package net.springboot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.ResourceBundleMessageSource;
-
-import java.util.Locale;
-
 
 @SpringBootApplication
 public class SpringbootBlog {
 
     public static void main(String[] args) {
-//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-//        messageSource.setBasenames("lang/res");
-//        System.out.println(messageSource.getMessage("hello", null , Locale.ENGLISH));
         SpringApplication.run(SpringbootBlog.class, args);
     }
 }
